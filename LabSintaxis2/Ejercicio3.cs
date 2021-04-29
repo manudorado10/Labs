8e4fe162d671456da7c8b7fbf82665815fd0627b
Manu Dorado <manudorado10@gmail.com>
2021-04-29 12:44:40 -0300

    Update labs

---
+++ LabSintaxis2/Ejercicio3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LabSintaxis2
+{
+    class Ejercicio3
+    {
+        static void Main2(string[] args)
+        { 
+            int cantIteraciones = 5;
+            string[] datos = new string[cantIteraciones];
+            for (int i = 0; i < cantIteraciones; i++) {
+                datos[i] = Console.ReadLine();
+            }
+            for (int x = cantIteraciones; x < 1 ; x--){
+                Console.WriteLine(datos[x]);
+            }
+        }
+    }
+}
